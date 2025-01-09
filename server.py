020a54864a67cb5e15022d721b81fa13e4895e48
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2025-01-09 14:08:58 +0000

    ss

--- server.py
+++ server.py
@@ -82,4 +82,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, log_level="info")
